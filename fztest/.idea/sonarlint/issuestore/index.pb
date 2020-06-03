29ef1682c63759a7ccfa7cf9c14efec6dff570ef
syn <s875087178@gmail.com>
2020-06-03 18:53:05 +0900

    分支测试

---
+++ fztest/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+=
+src/test.java,2\e\2e9f5287534e2ab86bcabfff7a3c5f9b053752ae
---
+++ fztest/src/test.java
@@ -0,0 +1,12 @@
+/**
+ * @author 宋永楠
+ * @version v1.0
+ * @Package PACKAGE_NAME
+ * @date 2020/6/3 18:43
+ * @Copyright @ msr
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("11111");
+    }
+}
